eb7cc4cfcceceebc23fd089515e05ea2d9e41609
yunwuxin <448901948@qq.com>
2024-03-06 12:21:30 +0800

    update

--- app/AppService.php
+++ app/AppService.php
@@ -3,6 +3,7 @@ declare (strict_types = 1);
 
 namespace app;
 
+use Symfony\Component\VarDumper\VarDumper;
 use think\App;
 use think\Service;
 
@@ -14,6 +15,10 @@ class AppService extends Service
     public function register()
     {
         // 服务注册
+        if (class_exists(VarDumper::class)) {
+            $_SERVER['VAR_DUMPER_FORMAT'] = 'tcp://host.docker.internal:9912';
+        }
+
         $this->app->resolving(function ($instance, App $container) {
             if ($instance instanceof BaseController) {
                 $container->invoke([$instance, 'initialize'], [], true);
